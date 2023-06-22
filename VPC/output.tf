de14443f93acca4a8e4bdc4a8fdb0058d7e1b200
Anatolii Shtyrka <128180505+Anatolich911@users.noreply.github.com>
2023-06-22 10:36:21 -0400

    changes

--- VPC/output.tf
+++ VPC/output.tf
@@ -1,6 +1,6 @@
-output = private_subnets {
+output private_subnets {
     value = module.vpc.private_subnets
 }
 
-output = public_subnets {
+output  public_subnets {
     value = module.vpc.public_subnets
