4f4e6b94cfd9bbfdb40c0db91a88863e9ae6a31e
Anatolii Shtyrka <128180505+Anatolich911@users.noreply.github.com>
2023-06-22 09:44:42 -0400

    rds

--- RDS/main.tf
+++ RDS/main.tf
@@ -88,3 +88,47 @@ resource "aws_autoscaling_policy" "scale_down" {
   cooldown               = "300"
   policy_type            = "SimpleScaling"
 }
+
+resource "aws_route53_record" "wordpress" {
+  zone_id = var.zone_id
+  name    = "wordpress.${var.domain}"
+  type    = "CNAME"
+  ttl     = "300"
+  records = [aws_alb.application-lb.dns_name]
+}
+variable domain {
+  type = string 
+
+}
+
+resource "aws_route53_record" "database_writer_endpoint" {
+  zone_id = var.zone_id
+  name    = "writer.${var.domain}"
+  type    = "CNAME"
+  ttl     = "300"
+  records = [aws_rds_cluster_instance.writer_instance[0].endpoint]
+}
+
+resource "aws_route53_record" "database_reader_endpoint1" {
+  zone_id = var.zone_id
+  name    = "reader1.${var.domain}"
+  type    = "CNAME"
+  ttl     = "300"
+  records = [aws_rds_cluster_instance.reader_instances[0].endpoint]
+}
+
+resource "aws_route53_record" "database_reader_endpoint2" {
+  zone_id = var.zone_id
+  name    = "reader2.${var.domain}"
+  type    = "CNAME"
+  ttl     = "300"
+  records = [aws_rds_cluster_instance.reader_instances[1].endpoint]
+}
+
+resource "aws_route53_record" "database_reader_endpoint3" {
+  zone_id = var.zone_id
+  name    = "reader3.${var.domain}"
+  type    = "CNAME"
+  ttl     = "300"
+  records = [aws_rds_cluster_instance.reader_instances[2].endpoint]
+}
