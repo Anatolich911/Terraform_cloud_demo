ac68a969ee60af6323098724251d9193e3422838
Anatolii Shtyrka <128180505+Anatolich911@users.noreply.github.com>
2023-06-22 11:34:11 -0400

    dar

--- RDS/main.tf
+++ RDS/main.tf
@@ -20,7 +20,7 @@ data "terraform_remote_state" "vpc" {
 #Create subnet group for RDS
 resource "aws_db_subnet_group" "project_db_subnet_group" {
   name       = "project_db_subnet_group"
-  subnet_ids = [aws_subnet.private1.id, aws_subnet.private2.id, aws_subnet.private3.id]
+  subnet_ids = data.terraform_remote_state.vpc.outputs.private_subnets
 }
 
 #Create cluster RDS
