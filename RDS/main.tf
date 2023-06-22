380ef463899914d212fa68c28ace8f2d6927a31a
Anatolii Shtyrka <128180505+Anatolich911@users.noreply.github.com>
2023-06-22 12:56:01 -0400

    deleting files

--- RDS/main.tf
+++ RDS/main.tf
@@ -14,9 +14,6 @@ data "terraform_remote_state" "vpc" {
 
 
 
-
-
-
 #Create subnet group for RDS
 resource "aws_db_subnet_group" "project_db_subnet_group" {
   name       = "project_db_subnet_group"
@@ -70,43 +67,7 @@ resource "aws_security_group" "database_sg" {
   }
 }
 
-# Create ASG
 
-resource "aws_autoscaling_group" "wordpress_asg" {
-  name                = "wordpress_asg"
-  max_size            = 99
-  min_size            = 1
-  health_check_type   = "ELB"
-  desired_capacity    = 1
-  target_group_arns   = [aws_alb_target_group.project-tg.arn]
-  vpc_zone_identifier = data.terraform_remote_state.vpc.outputs.public_subnets
-  launch_template {
-    id      = aws_launch_template.my_launch_template.id
-    version = "$Latest"
-  }
-  tag {
-    key                 = "Name"
-    value               = "asginstance"
-    propagate_at_launch = true
-  }
-}
-resource "aws_autoscaling_policy" "scale_up" {
-  name                   = "scale_up"
-  policy_type            = "SimpleScaling"
-  autoscaling_group_name = aws_autoscaling_group.wordpress_asg.name
-  adjustment_type        = "ChangeInCapacity"
-  scaling_adjustment     = "1"   # add one instance
-  cooldown               = "300" # cooldown period after scaling
-}
-
-resource "aws_autoscaling_policy" "scale_down" {
-  name                   = "asg-scale-down"
-  autoscaling_group_name = aws_autoscaling_group.wordpress_asg.name
-  adjustment_type        = "ChangeInCapacity"
-  scaling_adjustment     = "-1"
-  cooldown               = "300"
-  policy_type            = "SimpleScaling"
-}
 
 resource "aws_route53_record" "wordpress" {
   zone_id = var.zone_id
