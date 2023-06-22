1f47e19cdcbcf74c76fc9d6d55706d8eb5cc02f4
Anatolii Shtyrka <128180505+Anatolich911@users.noreply.github.com>
2023-06-22 11:07:39 -0400

    as

--- VPC/main.tf
+++ VPC/main.tf
@@ -25,7 +25,7 @@ module "vpc" {
 resource "aws_security_group" "my_sg" {
   name        = "my_sg"
   description = "Allow incoming HTTP Connections"
-  vpc_id      = aws_vpc.Project.id
+  vpc_id      = module.vpc.vpc_id
 
   ingress {
     from_port   = 80
